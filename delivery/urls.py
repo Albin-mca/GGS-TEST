b2838f9108c49a4d26c93c5ae6bf814390574c2d
albin2023 <albingeorge2023a@mca.ajce.in>
2023-05-16 10:19:12 +0530

    Delivery Boy

--- delivery/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('assign_delivery/', views.assign_delivery, name='assign_delivery'),
-    path('update_delivery_status/<int:delivery_id>/', views.update_delivery_status, name='update_delivery_status'),
-
-    path('deliveryboy_register/', views.deliveryboy_register, name='deliveryboy_register'),
-]
